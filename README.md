a8c4711ae0e0d347606e8162dd228f776a3cbc8c
jiyounyou <younny418@gmail.com>
2020-07-16 16:51:29 +0900

    backup

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 
 ### 1일차
 
+
 - 개발 입문자 오리엔테이션
 - 애플리케이션 개발 소개
 - 애플리케이션 개발 도구 설치
@@ -34,3 +35,15 @@
     - src/main/java/Hello2.java
     - src/main/java/com/eomcs/basic 폴더 생성
     - src/main/java/com/eomcs/basic/Hello2.java 생성
+    - 패키지 소속 클래스 실행하기
+      java -classpath [클래스파일있는폴더] [패키지명.클래스이름]
+
+### 4일차
+
+- 소스 파일고 클래스 블록, 클래스 파일의 관계
+    - com.eomcs.basic.ex01 패키지 예제
+- 주석 종류와 사용법
+    - com.eomcs.basic.ex02 패지키 예제
+- 자바 언어로 값을 표현하는 문법(Literal)
+
+    - com.eomcs.basic.ex03 패지키 예제
---
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex03/Exam0230.java
@@ -0,0 +1,32 @@
+// 주제 : 메모리 크기에 따른 표기법
+
+package com.eomcs.basic.ex03;
+
+
+class Exam0230 {
+    public static void main(String[] args){
+        // 출력 명령문
+        // -  System.out.println(값);
+
+        // 자바 정수값을 저장하는 메모리 기본 크기 : 4byte
+        System.out.println(2147483647);
+        System.out.println(-2147483648);
+
+        //System.out.println(2147483648);
+        //System.out.println(-2147483649);
+
+        // 8바이트 메모리를 사용하는 정수 리터럴
+        System.out.println(2147483648L);
+        System.out.println(-2147483649L);
+
+        System.out.println(1); // 4바이트 메모리를 사용하는 1
+        System.out.println(1L); // 8바이트 메모리를 사용하는 1
+
+        System.out.println(Integer.MAX_VALUE); // 4바이트 정수 최댓값
+        System.out.println(Integer.MIN_VALUE); // 4바이트 정수 최솟값
+
+        System.out.println(Long.MAX_VALUE); // 8바이트 정수 최댓값
+        System.out.println(Long.MIN_VALUE); // 8바이트 정수 최솟값
+
+    }
+}
