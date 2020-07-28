1a93172597cb3bb69cd2755ac089ebd5a2b28b63
jiyounyou <younny418@gmail.com>
2020-07-28 17:47:05 +0900

    backup

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex05/Test01.java
@@ -0,0 +1,8 @@
+package com.eomcs.basic.ex05;
+
+public class Test01 {
+  public static void main(String[] args) {
+    int data = 0b1111_1001_0111_1111;
+    System.out.println(Integer.toBinaryString(data & 0b0000_1111_1100_0000));
+  }
+}
